2543efcb698cf7b491e1d6b95f59fac9749f7104
Chris Dent <chris@indented.co.uk>
2020-12-18 10:12:52 +0000

    Removing Write-Host

--- 2020/18/Part2.ps1
+++ 2020/18/Part2.ps1
@@ -8,8 +8,6 @@ function eval {
         $match = [Regex]::Match($Problem, $problemRegex)
         $result = Invoke-Expression $match.Value
         $Problem = $Problem.Remove($match.Index, $match.Length).Insert($match.Index, $result)
-
-        # Write-Host "Add: $Problem"
     }
 
 
@@ -18,8 +16,6 @@ function eval {
         $match = [Regex]::Match($Problem, $problemRegex)
         $result = Invoke-Expression $match.Value
         $Problem = $Problem.Remove($match.Index, $match.Length).Insert($match.Index, $result)
-
-        # Write-Host "Multiply: $Problem"
     }
 
     $Problem
@@ -29,14 +25,9 @@ $groupRegex = '\((\d+(?: [*+] \d+)+)\)'
 
 gc $PSScriptRoot\input.txt | %{
     $problem = $_
-
-    # Write-Host $problem
-
     do {
         [Regex]::Matches($problem, $groupRegex) | Sort-Object Index -Descending | %{
             $result = eval $_.Groups[1]
-
-            # Write-Host "Removing $($_.Groups[1]) and Inserting $result"
             $problem = $problem.Remove(
                 $_.Index,
                 $_.Length
