838e058c04607ecd49fb78188c6f3e30c68be7f9
Chris Dent <chris@indented.co.uk>
2021-12-04 09:43:42 +0000

    Adds 2021 Day 4

---
+++ 2021/04/Part1.ps1
@@ -0,0 +1,52 @@
+$draw, $boards = (gc "$PSScriptRoot\input.txt" -Raw) -split '(\r?\n){2}'
+$draw = [System.Collections.Generic.Queue[string]]($draw -split ',')
+$boards = $boards.Trim() -match '.+'
+
+$index = @{}
+$boardNumber = 0
+$boards = foreach ($board in $boards) {
+    $boardInfo = [PSCustomObject]@{
+        Number = ($boardNumber++)
+        Board  = [string[][]]::new(5, 5)
+        Index  = @{}
+        Marked = @{
+            x = @{}
+            y = @{}
+        }
+        Total  = 0
+    }
+    $boardInfo
+
+    $y = 0
+    foreach ($row in $board -split '\r?\n') {
+        $x = 0
+        foreach ($column in $row.Trim() -split '\s+') {
+            $value = $column
+
+            $boardInfo.Total += $value -as [int]
+            $boardInfo.Board[$x][$y] = $value
+            $boardInfo.Index[$value] = $x, $y
+            $index[$value] += @($boardInfo)
+            $x++
+        }
+        $y++
+    }
+}
+
+:game do {
+    $value = $draw.Dequeue()
+    if ($index.Contains($value)) {
+        foreach ($board in $index[$value]) {
+            $board.Total -= $value
+
+            $x, $y = $board.Index[$value]
+            $board.Marked['x'][$x]++
+            $board.Marked['y'][$y]++
+
+            if ($board.Marked['x'][$x], $board.Marked['y'][$y] -contains 5) {
+                $board.Total * $value
+                break game
+            }
+        }
+    }
+} while ($draw.Count)
