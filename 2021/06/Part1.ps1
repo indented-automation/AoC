8e5007491f5409b95087e80fab5a2c0e83884b83
Chris Dent <chris@indented.co.uk>
2021-12-06 09:10:36 +0000

    Adds 2021 Day 6

---
+++ 2021/06/Part1.ps1
@@ -0,0 +1,44 @@
+$days = 80
+
+$lifecycleMain = @{
+    '0' = 0
+    '1' = 0
+    '2' = 0
+    '3' = 0
+    '4' = 0
+    '5' = 0
+    '6' = 0
+}
+$lifecycleFirst = $lifecycleMain + @{
+    '7' = 0
+    '8' = 0
+}
+
+(gc $PSScriptRoot\input.txt) -split ',' | ForEach-Object {
+    $lifecycleMain[$_]++
+}
+
+for ($day = 1; $day -le $days; $day++) {
+    $lifecycleMainState = $lifecycleMain.Clone()
+    $lifecycleFirstState = $lifecycleFirst.Clone()
+
+    for ($stage = 0; $stage -le 8; $stage++) {
+        if ($stage -gt 0) {
+            $lifecycleFirst[$stage - 1 -as [string]] = $lifecycleFirstState[$stage -as [string]]
+            if ($lifecycleMain.Contains($stage -as [string])) {
+                $lifecycleMain[$stage - 1 -as [string]] = $lifecycleMainState[$stage -as [string]]
+            }
+        } else {
+            $lifecycleFirst['8'] = $lifecycleFirstState['0'] + $lifecycleMainState['0']
+            $lifecycleMain['6'] = $lifecycleFirstState['0'] + $lifecycleMainState['0']
+            $lifecycleFirst['0'] = 0
+        }
+    }
+
+    $total = 0
+    $lifecycleFirst.Keys | Sort-Object { [int]$_ } -Descending | ForEach-Object {
+        $value = $lifecycleFirst[$_] + $lifecycleMain[$_]
+        $total += $value
+    }
+}
+$total
