ef50f2429fa4a965a45f1d8c3a3767b0cb91301c
Chris Dent <chris@indented.co.uk>
2021-12-07 10:22:26 +0000

    Fixes exit condition

--- 2021/07/Part2.ps1
+++ 2021/07/Part2.ps1
@@ -11,15 +11,19 @@ foreach ($position in $positions) {
     }
 }
 
-$cost = [int]::MaxValue
+$cost = $last = [int]::MaxValue
 for ($target = $lowest; $target -le $highest; $target++) {
     $runCost = 0
     foreach ($position in $positions) {
         $value = [Math]::Abs($target - $position)
         $runCost += ($value / 2) * ($value + 1)
     }
+    if ($last -lt $runCost) {
+        break
+    }
     if ($runCost -lt $cost) {
         $cost = $runCost
     }
+    $last = $runCost
 }
 $cost
