1e1df622a5a461ab8380cfcf130b11a442f34e92
Chris Dent <chris@indented.co.uk>
2022-12-02 08:52:17 +0000

    Regex is still slow

--- 2022/02/Part1.ps1
+++ 2022/02/Part1.ps1
@@ -3,20 +3,23 @@ enum Move {
     Paper
     Scissors
 }
+
 $total = 0
 foreach ($move in [System.IO.File]::ReadAllLines("$PSScriptRoot\input.txt")) {
-    [Move]$opponent, [Move]$player = switch -regex ($move -split '\s+') {
-        '[ax]' { 'Rock' }
-        '[by]' { 'Paper' }
-        '[cz]' { 'Scissors' }
+    [Move]$opponent, [Move]$player = switch ($move -split '\s+') {
+        'a' { 'Rock'; continue }
+        'b' { 'Paper'; continue }
+        'c' { 'Scissors'; continue }
+        'x' { 'Rock'; continue }
+        'y' { 'Paper'; continue }
+        'z' { 'Scissors' }
     }
     $outcome = switch ($opponent) {
         { $_ -eq $player } { 3; break }
-        'Rock'     { 6 * ($player -eq 'Paper') }
-        'Paper'    { 6 * ($player -eq 'Scissors') }
+        'Rock'     { 6 * ($player -eq 'Paper'); break }
+        'Paper'    { 6 * ($player -eq 'Scissors'); break }
         'Scissors' { 6 * ($player -eq 'Rock') }
     }
     $total += $outcome + $player
 }
 $total
-
