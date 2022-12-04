a7dfa70d175f1bf8c61143904f736dae2cf62a2c
Chris Dent <chris@indented.co.uk>
2022-12-04 10:03:12 +0000

    Adds "duh" variant

--- 2022/04/Part1_2.ps1
+++ 2022/04/Part1_2.ps1
@@ -4,16 +4,12 @@ $count = 0
 foreach ($assignment in $data) {
     $sections = foreach ($section in $assignment -split ',') {
         $start, $end = $section -split '-' -as [int[]]
-        $area = 0
-        for ($i = $start; $i -le $end; $i++) {
-            $area += [BigInt]1 -shl ($i - 1)
-        }
-        $area
+        @{ Start = $start; End = $end }
     }
 
-    if (($sections[0] -band $sections[1]) -in $sections) {
-        Write-Host $assignment
-
+    if ($sections[0].Start -ge $sections[1].Start -and $sections[0].End -le $sections[1].End) {
+        $count++
+    } elseif ($sections[1].Start -ge $sections[0].Start -and $sections[1].End -le $sections[0].End) {
         $count++
     }
 }
