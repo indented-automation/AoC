5dfc2e832bba353301582cfdc3b3827186a84527
Chris Dent <chris@indented.co.uk>
2022-12-04 09:39:20 +0000

    Adds 2022 Day 4

---
+++ 2022/04/Part2.ps1
@@ -0,0 +1,18 @@
+$data = [System.IO.File]::ReadAllLines("$PSScriptRoot\input.txt")
+
+$count = 0
+foreach ($assignment in $data) {
+    $sections = foreach ($section in $assignment -split ',') {
+        $start, $end = $section -split '-' -as [int[]]
+        $area = 0
+        for ($i = $start; $i -le $end; $i++) {
+            $area += [BigInt]1 -shl ($i - 1)
+        }
+        $area
+    }
+
+    if ($sections[0] -band $sections[1]) {
+        $count++
+    }
+}
+$count
