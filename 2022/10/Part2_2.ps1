5046554fc610d4c786d4e696e9d4db7c86eacca8
Chris Dent <chris@indented.co.uk>
2022-12-10 11:18:23 +0000

    Just because it makes part 2 really fast

---
+++ 2022/10/Part2_2.ps1
@@ -0,0 +1,34 @@
+$data = [System.IO.StreamReader][System.IO.File]::OpenRead("$PSScriptRoot\input.txt")
+
+$screen = [System.Text.StringBuilder]::new()
+1..6 | ForEach-Object {
+    $null = $screen.AppendLine('........................................')
+}
+
+$next = 0
+$x = $cycle = 1
+$sprite = $pixel = 0
+do {
+    $position = $pixel % 40
+    if ($position -ge $sprite - 1 -and $position -le $sprite + 1) {
+        # CrLf
+        $lineBreakChars = 2 * (($pixel - $position) / 40)
+        $null = $screen.Replace('.', '#', ($pixel + $lineBreakChars), 1)
+    }
+
+    if ($next) {
+        $x += $next
+        $next = 0
+        $sprite = $x
+    } else {
+        $operation = $data.ReadLine()
+        if ($operation -match '^addx (-?\d+)$') {
+            $next = $matches[1]
+        }
+    }
+
+    $pixel++
+    $cycle++
+} until ($data.EndOfStream)
+
+$screen.ToString()
