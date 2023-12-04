d9fc1e42ada6508ca55d990798128feef8a87672
Chris Dent <chris@indented.co.uk>
2023-12-04 10:07:30 +0000

    Remove pointless conditions

--- 2023/04/Part2.ps1
+++ 2023/04/Part2.ps1
@@ -1,26 +1,26 @@
 using namespace System.Collections.Generic
 
 $cards = foreach ($card in [System.IO.File]::ReadAllLines("$PSScriptRoot\input.txt")) {
-    [int]$number, $win, $hand = $card -split ':\s+|\s\|\s+' -replace '^card\s+'
+    $null, $win, $hand = $card -split ':\s+|\s\|\s+'
 
     $matching = [HashSet[int]]($hand -split '\s+')
     $matching.IntersectWith(
         [HashSet[int]]($win -split '\s+')
     )
 
-    [PSCustomObject]@{ Card = $number; MatchCount = $matching.Count; Copies = 1 }
+    @{ MatchCount = $matching.Count; Copies = 1 }
 }
 
 $sum = 0
-for ($i = 0; $i -lt $cards.Count -and $i -le $cards[-1].Card; $i++) {
+for ($i = 0; $i -lt $cards.Count; $i++) {
     $current = $cards[$i]
 
-    if ($current.MatchCount) {
-        for ($j = $i + 1; $j -le $i + $current.MatchCount; $j++) {
-            $cards[$j].Copies += $current.Copies
+    if ($current['MatchCount']) {
+        for ($j = $i + 1; $j -le $i + $current['MatchCount']; $j++) {
+            $cards[$j]['Copies'] += $current['Copies']
         }
     }
 
-    $sum += $current.Copies
+    $sum += $current['Copies']
 }
 $sum
