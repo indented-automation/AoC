3a0349c65a7df477c6110bf89a16538b8ac7d4a6
Chris Dent <chris@indented.co.uk>
2024-12-07 20:31:37 +0000

    Just mucking around

--- 2024/07/Part2.ps1
+++ 2024/07/Part2.ps1
@@ -46,16 +46,23 @@ foreach ($equation in $equations -replace '.+:\s*') {
     $max = [Math]::Max($max, $size)
 }
 
+$stopWatch = [System.Diagnostics.StopWatch]::StartNew()
 $solverTable = @{}
 for ($i = $min; $i -le $max; $i++) {
+    Write-Host ('<{0}> [{1,2}/{2,2}] Calculating permutations' -f $stopWatch.Elapsed, $i, $max)
     $solverTable[$i] = Get-Permutation -Values $operators -Length $i
 }
 
 $sum = 0l
+$x = 0
 foreach ($equation in $equations) {
+    $x++
+    Write-Host ('<{0}> [{1,3}/{2,3}] Solving {3,-50}' -f $stopWatch.Elapsed, $x, $equations.Count, $equation) -NoNewline
+
     [long]$expected, [long[]]$values = $equation -split ':?\s'
 
     $operatorSets = $solverTable[$values.Count - 1]
+    $solved = $false
     :operators
     foreach ($operators in $operatorSets) {
         $i = 0
@@ -68,11 +75,17 @@ foreach ($equation in $equations) {
         }
 
         if ($result -eq $expected) {
-            # Write-Host ('{0}: Solved {1}' -f ($operators -join ' '), $equation) -ForegroundColor Green
+            $solved = $true
             $sum += $expected
             break
         }
     }
 
+    if ($solved) {
+        Write-Host ' OK!' -ForegroundColor Green
+    } else {
+        Write-Host
+    }
 }
+Write-Host ('Completed in {0}' -f $stopWatch.Elapsed)
 $sum
