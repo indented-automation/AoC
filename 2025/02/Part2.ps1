4b81dabaaefa5b58d97948e7d534329401703419
Chris Dent <chris@indented.co.uk>
2025-12-02 09:29:56 +0000

    Adds 2025 day 2 Part 2

---
+++ 2025/02/Part2.ps1
@@ -0,0 +1,15 @@
+$data = Get-Content input.txt -Raw
+
+$sum = 0l
+foreach ($range in $data -replace '\r?\n' -split ',') {
+    $first, $last = $range -split '-' -as [int64[]]
+
+    for ($value = $first; $value -le $last; $value++) {
+        if ($value -match '^(\d+)\1+$') {
+            $value
+
+            $sum += $value
+        }
+    }
+}
+$sum
