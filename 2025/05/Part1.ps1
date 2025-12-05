b82efa9799a54f27e8068462a4ce407eefa464c2
indented-automation <chris@indented.co.uk>
2025-12-05 10:00:27 +0000

    Adds 2025 day 5

---
+++ 2025/05/Part1.ps1
@@ -0,0 +1,80 @@
+using namespace System.Collections.Generic
+using namespace System.IO
+
+[CmdletBinding()]
+param (
+    [switch]
+    $Sample
+)
+
+class Range {
+    [long]
+    $Start
+
+    [long]
+    $End
+
+    [Range[]]
+    $OverlapsWith
+
+    [bool]
+    $IsMerged
+
+    [string] ToString() {
+        return '{0}-{1}' -f $this.Start, $this.End
+    }
+}
+
+$fileName = 'input.txt'
+if ($Sample) {
+    $fileName = 'sample.txt'
+}
+
+$data = [File]::ReadAllText([Path]::Combine($PSScriptRoot, $fileName))
+
+$fresh, $available = $data -split '(\r?\n){2,}'
+
+$fresh = $fresh.Trim() -split '\r?\n'
+$available = $available.Trim() -split '\r?\n' -ne '' -as [long[]]
+
+[List[Range]]$ranges = foreach ($range in $fresh) {
+    $start, $end = $range -split '-'
+    @{ Start = $start; End = $end }
+}
+
+do {
+    $foundOverlap = $false
+    $ranges = foreach ($a in $ranges) {
+        if ($a.IsMerged) {
+            continue
+        }
+
+        foreach ($b in $ranges) {
+            if ($a -eq $b) {
+                continue
+            }
+
+            if (($a.Start -ge $b.Start -and $a.Start -le $b.End) -or
+                ($a.End -ge $b.Start -and $a.End -le $b.End)
+            ) {
+                $foundOverlap = $true
+                $a.Start = [Math]::Min($a.Start, $b.Start)
+                $a.End = [Math]::Max($a.End, $b.End)
+                $b.IsMerged = $true
+            }
+        }
+
+        $a
+    }
+} while ($foundOverlap)
+
+$count = 0
+foreach ($ingredient in $available) {
+    foreach ($range in $ranges) {
+        if ($ingredient -ge $range.Start -and $ingredient -le $range.End) {
+            $count++
+            break
+        }
+    }
+}
+$count
